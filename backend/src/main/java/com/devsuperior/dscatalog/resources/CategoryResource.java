3a404ec628e7b8c2ba1e6d6ea5ec37a4171f0d3b
rqguzman <rafael.guzman@outlook.com>
2021-01-15 17:16:27 -0300

    Add insert category endpoint

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,17 +1,13 @@
 package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
-import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.websocket.server.PathParam;
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -31,6 +27,19 @@ public class CategoryResource {
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
         CategoryDTO catDTO = categoryService.findById(id);
         return ResponseEntity.ok().body(catDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO catDTO) {
+
+        catDTO = categoryService.insert(catDTO);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                                        .path("/{id}")
+                                        .buildAndExpand(catDTO.getId())
+                                        .toUri();
+
+        return ResponseEntity.created(uri).body(catDTO);
     }
 
 }
