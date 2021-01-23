a45b34d14e83afc9d416000e726dbe1efcfdc085
rqguzman <rafael.guzman@outlook.com>
2021-01-23 16:53:43 -0300

    :sparkles:feat(Home): Add Home view

--- front-web/src/pages/home/index.tsx
+++ front-web/src/pages/home/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import './styles.scss';
+import { ReactComponent as MainImage } from '../../core/assets/images/main-image.svg';
+import ButtonIcon from '../../core/components/ButtonIcon';
+import { Link } from 'react-router-dom';
 
 const Home = () => (
 
-  <div>
-    <h1>Home</h1>
+  <div className="home-container">
+    <div className="home-content row">
+      <div className="col-6">
+        <h1 className="text-title">Know the best <br /> product catalog</h1>
+        <p className="text-subtitle">
+          We'll help you to find the best <br /> products available on the market.
+        </p>
+        <Link to="/catalog">
+          <ButtonIcon text="Begin your search now" />
+        </Link>
+      </div>
+      <div className="col-6">
+        <MainImage className="main-image" />
+      </div>
+    </div>
   </div>
 );
 
