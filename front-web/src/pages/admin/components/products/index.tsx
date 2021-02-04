b99d5878d52e3d5fde020c109a57e6081ee48ea5
rqguzman <rafael.guzman@outlook.com>
2021-02-04 17:58:39 -0300

    :sparkles:feat(Admin): Add Admin's basic form template

--- front-web/src/pages/admin/components/products/index.tsx
+++ front-web/src/pages/admin/components/products/index.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import Form from './form';
+import List from './list';
 
 const Products = () => {
     return (
         <div>
-            <Link to="/admin/products" className="mr-5">
-                List Products
-            </Link>
-            <Link to="/admin/products/create" className="mr-5">
-                Create Product
-            </Link>
-            <Link to="/admin/products/10" className="mr-5">
-                Edit Product
-            </Link>
             <Switch>
                 <Route path="/admin/products" exact>
-                    <h1>List products</h1>
+                    <List />
                 </Route>
                 <Route path="/admin/products/create">
-                    <h1>Create new product</h1>
+                    <Form />
                 </Route>
                 <Route path="/admin/products/:productId">
                     <h1>Edit product</h1>
---
+++ front-web/src/pages/admin/components/products/list/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './styles.scss';
+
+const List = () => {
+
+    const history = useHistory();
+
+    const handleCreate = () => {
+        history.push('/admin/products/create');
+    }
+
+    return (
+        <div className="admin-products-list">
+            <button className="btn btn-primary btn-lg" onClick={handleCreate}>
+                ADD
+            </button>
+        </div>
+    );
+};
+
+export default List;
