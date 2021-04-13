18dcecc9e63f03a583960f4f2a3839d044571054
rqguzman <rafael.guzman@outlook.com>
2021-04-13 14:27:51 -0300

    :sparkles: Feat(Login): Compose login form layout

--- front-web/src/pages/auth/components/login/index.tsx
+++ front-web/src/pages/auth/components/login/index.tsx
@@ -1,4 +1,6 @@
+import ButtonIcon from 'core/components/ButtonIcon';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import AuthCard from '../card';
 import './styles.scss';
 
@@ -7,7 +9,30 @@ const Login = () => {
         <div>
             <AuthCard title="login">
                 <form className="login-form">
-                    <h1>Login form</h1>
+                    <input 
+                        type="email"
+                        className="form-control input-base margin-bottom-30"
+                        placeholder="email"
+                    />
+                    <input 
+                        type="password "
+                        className="form-control input-base"
+                        placeholder="password"
+                    />
+                    <Link to="/admin/auth/recover" className="login-link-recover">
+                        Forgotten your login details?
+                    </Link>
+                    <div className="login-submit">
+                        <ButtonIcon text="login" />
+                    </div>
+                    <div className="text-center">
+                        <span className="not-registered">
+                            Not registered?
+                        </span>
+                        <Link to="/admin/auth/register" className="login-link-register">
+                            REGISTER
+                        </Link>
+                    </div>
                 </form>
             </AuthCard>
         </div>
